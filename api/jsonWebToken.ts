ab33ac2f229e46702d581c54d9653ae36d0158d2
fibo <casati_gianluca@yahoo.it>
2019-06-08 23:55:17 +0200

    jwt promises

--- api/jsonWebToken.ts
+++ api/jsonWebToken.ts
@@ -1,9 +1,5 @@
 import * as jwt from "jsonwebtoken"
 import no from "not-defined"
-import { promisify } from "util"
-
-const jwtSign = promisify(jwt.sign)
-const jwtVerify = promisify(jwt.verify)
 
 import {
   MissingJwtSecretError,
@@ -27,25 +23,33 @@ if (no(secret)) {
   throw new MissingJwtSecretError()
 }
 
-export async function sign(payload: IJwtPayload): Promise<IJwtData> {
-  // Set expiration to one month.
-  const days = 30
-  const expiresIn = 86400 * days
+export function sign(payload: IJwtPayload): Promise<IJwtData> {
+  return new Promise((resolve, reject) => {
+    // Set expiration to one month.
+    const days = 30
+    const expiresIn = 86400 * days
 
-  const expirationDate = new Date()
-  expirationDate.setDate(expirationDate.getDate() + days)
+    const expirationDate = new Date()
+    expirationDate.setDate(expirationDate.getDate() + days)
+    const expiresAt = expirationDate.toISOString()
 
-  try {
-    return await jwtSign(payload, secret, { algorithm, expiresIn })
-  } catch (error) {
-    throw error
-  }
+    jwt.sign(payload, secret, { algorithm, expiresIn }, (error, token) => {
+      if (error) {
+        reject(error)
+      } else {
+        resolve({
+          expiresAt,
+          token,
+        })
+      }
+    })
+  })
 }
 
-export async function verify(token): Promise<IJwtPayload> {
-  const payload = await jwtVerify(token, secret, { algorithms: [algorithm] })
-
-  return payload
+export function verify(token): Promise<IJwtPayload> {
+  return new Promise((resolve, reject) => {
+    jwt.verify(token, secret, { algorithms: [algorithm] }, (error, data) => error ? reject(error) : resolve(data))
+  })
 }
 
 export async function verifyHeaders({ Authorization }) {
